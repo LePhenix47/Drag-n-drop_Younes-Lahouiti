d492f5c7c8e4f49451f333cc399a9e0ce8546f80
Younes Lahouiti <loller763@gmail.com>
2023-05-29 10:20:05 +0200

    Addition of the Drag and drop feature

--- src/components/draggable-element.component.ts
+++ src/components/draggable-element.component.ts
@@ -1,46 +1,100 @@
+import { log } from "../utils/functions/console.functions";
 import {
+  addClass,
   getAttribute,
   modifyAttribute,
+  removeClass,
+  selectQuery,
 } from "../utils/functions/dom.functions";
 import {
   cssReset,
   darkThemeVariables,
+  jsClasses,
   lightThemeVariables,
 } from "../utils/vairables/web-component.variables";
 
 const draggableTemplateElement = document.createElement("template");
 
 const draggableTemplateCssStyle = /* css */ `
- .draggable-element{}
 
- .draggable-element__icon-container{}
+.draggable-element {
+    align-items: center;
+    background-color: var(--bg-primary);
+    border-radius: calc(var(--container-border-radius) - var(--padding-container));
+    display: flex;
+    height: 50px;
+    justify-content: space-between;
+    overflow: hidden;
+    width: 100%;
+    padding: 5px;
+}
 
- .draggable-element__icon{}
+.draggable-element.dragging{
+  filter: invert(100%) hue-rotate(180deg);
+}
 
- .draggable-element__text-image-container{}
+.draggable-element__image {
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+    height: 35px;
+    -o-object-fit: cover;
+    object-fit: cover;
+    -o-object-position: center;
+    object-position: center;
+}
 
- .draggable-element__image{}
+.draggable-element__icon-container {
+  background-color: var(--bg-primary);
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  filter: invert(100%) hue-rotate(180deg);
+  gap: 0;
+  height: 100%;
+  width: 15%;
+}
 
- .draggable-element__paragraph{}
+.draggable-element__icon-container:hover {
+    cursor: grab;
+}
+
+.draggable-element__icon-container:active {
+    cursor: grabbing;
+}
+
+.draggable-element__icon {
+    scale: 250%;
+}
+
+.draggable-element__text-image-container {
+    align-items: center;
+    display: flex;
+    gap: 20px;
+    width: 75%;
+}
 `;
 const draggableTemplateHtmlContent = /*html */ `
- <div class="draggable-element">
-    <div class="draggable-element__icon-container">
-        <svg class="draggable-element__icon no-pointer-events" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor">
-            <path
-                d="M10 13a1 1 0 100-2 1 1 0 000 2zm-4 0a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zM6 5a1 1 0 100-2 1 1 0 000 2z">
-            </path>
-        </svg>
-    </div>
-    <div class="draggable-element__text-image-container">
-        <img class="draggable-element__image" src="../../public/jpg/random-photo.jpg" />
-        <p class="draggable-element__paragraph">Name 1234</p>
-    </div>
- </div>
+  <div class="draggable-element">
+      <div class="draggable-element__icon-container">
+          <svg class="draggable-element__icon no-pointer-events" xmlns="http://www.w3.org/2000/svg" width="16"
+              height="16" fill="var(--color-primary)">
+              <path
+                  d="M10 13a1 1 0 100-2 1 1 0 000 2zm-4 0a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zM6 5a1 1 0 100-2 1 1 0 000 2z">
+              </path>
+          </svg>
+      </div>
+      <div class="draggable-element__text-image-container">
+          <img class="draggable-element__image" src="../../public/jpg/random-photo.jpg" />
+          <p class="draggable-element__paragraph">Name 1234</p>
+      </div>
+  </div>
 `;
 
 draggableTemplateElement.innerHTML = /*html */ `
   <style>
+    ${jsClasses}
     ${cssReset}
     ${lightThemeVariables}
     ${darkThemeVariables}
@@ -62,6 +116,13 @@ class DraggableElement extends HTMLElement {
     const clonedTemplate = draggableTemplateElement.content.cloneNode(true);
     //We add it as a child of our web component
     shadowRoot.appendChild(clonedTemplate);
+
+    //We bind the this keyword to have access to the shadow root
+    this.enableDragging = this.enableDragging.bind(this);
+    this.disableDragging = this.disableDragging.bind(this);
+
+    this.addDraggingClass = this.addDraggingClass.bind(this);
+    this.removeDraggingClass = this.removeDraggingClass.bind(this);
   }
 
   get name() {
@@ -77,13 +138,65 @@ class DraggableElement extends HTMLElement {
     return ["name"];
   }
 
-  connectedCallback() {}
+  private enableDragging(event: PointerEvent) {
+    modifyAttribute(this, "draggable", true);
+  }
+  private disableDragging(event: PointerEvent) {
+    modifyAttribute(this, "draggable", false);
+  }
 
-  disconnectedCallback() {}
+  private addDraggingClass(event: DragEvent) {
+    addClass(this, "dragging");
+  }
+
+  private removeDraggingClass(event: DragEvent) {
+    const iconContainer: HTMLElement = selectQuery(
+      ".draggable-element__icon-container",
+      this.shadowRoot
+    );
+
+    iconContainer.click();
+
+    removeClass(this, "dragging");
+  }
+
+  connectedCallback() {
+    const iconContainer: HTMLElement = selectQuery(
+      ".draggable-element__icon-container",
+      this.shadowRoot
+    );
+
+    iconContainer.addEventListener("pointerdown", this.enableDragging);
+    iconContainer.addEventListener("pointerup", this.disableDragging);
+
+    this.addEventListener("dragstart", this.addDraggingClass);
+    this.addEventListener("dragend", this.removeDraggingClass);
+  }
+
+  disconnectedCallback() {
+    const iconContainer: HTMLElement = selectQuery(
+      ".draggable-element__icon-container",
+      this.shadowRoot
+    );
+
+    iconContainer.removeEventListener("pointerdown", this.enableDragging);
+    iconContainer.removeEventListener("pointerup", this.disableDragging);
+
+    this.removeEventListener("dragstart", this.addDraggingClass);
+    this.removeEventListener("dragend", this.removeDraggingClass);
+  }
 
   attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
       case "name": {
+        const paragraph: HTMLParagraphElement = selectQuery(
+          ".draggable-element__paragraph",
+          this.shadowRoot
+        );
+
+        log(paragraph);
+
+        paragraph.textContent = newValue;
         //…
         break;
       }
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,71 @@ import { log } from "./utils/functions/console.functions";
 
 //Web components
 import "./components/draggable-element.component";
+import {
+  selectByClass,
+  selectQuery,
+  selectQueryAll,
+} from "./utils/functions/dom.functions";
 
 log("Hello world!");
+
+function addContainerEventListeners() {
+  const containersArray: HTMLElement[] = selectByClass("index__container");
+
+  for (const container of containersArray) {
+    container.addEventListener("dragover", handleDraggingElementDragOver);
+  }
+}
+addContainerEventListeners();
+
+function handleDraggingElementDragOver(event: DragEvent) {
+  event.preventDefault();
+
+  const mouseYPosition: number = event.clientY;
+
+  const draggedDraggable: HTMLElement = selectQuery(".dragging");
+
+  const closestElement: HTMLElement | null = getClosestElementFromMouse(
+    this,
+    mouseYPosition
+  );
+  //
+  const hasNoAfterElement: boolean = closestElement === null;
+  if (hasNoAfterElement) {
+    this.appendChild(draggedDraggable);
+  } else {
+    this.insertBefore(draggedDraggable, closestElement);
+  }
+}
+
+function getClosestElementFromMouse(
+  container: HTMLElement,
+  mouseYPosition: number
+): HTMLElement | null {
+  const staticDraggablesArray: HTMLElement[] = selectQueryAll(
+    ".index__draggable:not(.dragging)",
+    container
+  );
+
+  //We initialize 2 vairable to get the closet element
+  //and the the closest offset nearing the most 0
+  let closestElement: HTMLElement | null = null;
+  let closestOffset: number = Number.NEGATIVE_INFINITY;
+
+  for (const staticDraggable of staticDraggablesArray) {
+    const draggableRect: DOMRect = staticDraggable.getBoundingClientRect();
+    const { top, height } = draggableRect;
+
+    const currentOffset: number = mouseYPosition - (top + height / 2);
+
+    const isAboveAfterElement: boolean = currentOffset < 0;
+    const hasGreaterOffset: boolean = currentOffset > closestOffset;
+
+    if (isAboveAfterElement && hasGreaterOffset) {
+      closestOffset = currentOffset;
+      closestElement = staticDraggable;
+    }
+  }
+
+  return closestElement;
+}
